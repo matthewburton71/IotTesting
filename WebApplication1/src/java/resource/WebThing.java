4f705dad646016ca01505fd5adbceb0a17b38e50
matthewburton71 <matthewburton71@gmail.com>
2016-03-20 19:10:14 -0600

    Adding the code

---
+++ WebApplication1/src/java/resource/WebThing.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resource;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author matthew
+ */
+public class WebThing {
+
+    public WebThing() {
+        createdAt = currentTime();
+        updatedAt = currentTime();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        updatedAt = currentTime();
+        return updatedAt;
+    }
+    
+    private String id = "myPi2";
+    String name = "My Rastperry Pi";
+    String description = "This is my Rastpery Pt rev2";
+    String createdAt = "";
+    String updatedAt = "";
+//    String updatedAt = "";
+    
+    
+    /// helper meathods
+    private String currentTime () {
+        String thisMoment = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmX")
+                              .withZone(ZoneOffset.UTC)
+                              .format(Instant.now());
+        return thisMoment;
+    }
+}
---
+++ WebApplication1/src/java/resource/thing1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resource;
+
+import com.google.gson.Gson;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author matthew
+ */
+@Path("thing")
+public class thing1 {
+
+    @Context
+    private UriInfo context;
+    
+    Gson gson;
+    WebThing thing;
+
+    /**
+     * Creates a new instance of thing1
+     */
+    public thing1() {
+        System.out.println("resource.thing1.<init>()");
+        thing = new WebThing();
+        gson = new Gson();
+    }
+
+    /**
+     * Retrieves representation of an instance of resource.thing1
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        System.out.println("resource.thing1.getJson()");
+        return gson.toJson(thing);
+    }
+    
+    /**
+     * PUT method for updating or creating an instance of thing1
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+        System.out.println("resource.thing1.putJson()");
+        System.out.println("content: " + content);
+    }
+}
